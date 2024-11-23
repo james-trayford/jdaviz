acc61b1a74246566831babb75cc4a25157e7ec98
James Trayford <j.w.trayford@port.ac.uk>
2024-11-23 20:47:16 +0000

    rollback patch

--- jdaviz/core/validunits.py
+++ jdaviz/core/validunits.py
@@ -207,15 +207,15 @@ def check_if_unit_is_per_solid_angle(unit, return_unit=False):
             # to check type
             new_unit_str = ' '.join(i).translate(str.maketrans('', '', '()'))
             new_unit = u.Unit(new_unit_str)
-            if new_unit.physical_type == 'solid angle' or new_unit == PIX2 or new_unit_str == 'spaxel':  # noqa
+            if new_unit.physical_type == 'solid angle':
                 if return_unit:  # area units present and requested to be returned
                     return new_unit
                 return True  # area units present but not requested to be returned
             # square pixel should be considered a square angle unit
-            # if new_unit == PIX2:
-            #     if return_unit:
-            #         return new_unit
-            #     return True
+            if new_unit == PIX2:
+                if return_unit:
+                    return new_unit
+                return True
 
     # in the case there are no area units, but return units were requested
     if return_unit:
